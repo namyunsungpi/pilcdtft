1204fbc1de0eeef47a824618d3e23ed0f3a458ef
Seungrok Han <muster22@naver.com>
2014-07-20 15:34:35 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -77,8 +77,8 @@ if [ ! -d "$DIRECTORY/rpi-fbcp" ]; then
     echo "rpi-firmware rpi-update Process" >> $LOGFILE
     sudo wget https://raw.github.com/Hexxeh/rpi-update/master/rpi-update -O /usr/bin/rpi-update && sudo chmod +x /usr/bin/rpi-update
     sudo mv /lib/modules/$(uname -r) /lib/modules/$(uname -r).bak
-#    sudo REPO_URI=https://github.com/notro/rpi-firmware UPDATE_SELF=0 rpi-update
-    sudo REPO_URI=https://github.com/notro/rpi-firmware rpi-update
+    sudo REPO_URI=https://github.com/notro/rpi-firmware UPDATE_SELF=0 rpi-update
+    #sudo REPO_URI=https://github.com/notro/rpi-firmware rpi-update
     
     echo "rpi-firmware rpi-update Done" >> $LOGFILE
 
